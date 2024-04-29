f57cb6d37bc85e2c86a35282bd7451d97d78c3f6
Kylie Ainslie <ainslie.kylie@gmail.com>
2024-04-29 10:45:15 +0200

    add example for weighted.var()

--- R/weighted_var.R
+++ R/weighted_var.R
@@ -1,10 +1,27 @@
-# function for weighted variance, to be used in E-step
-# weighted variance
+#' Calculate weighted variance
+#'
+#' This function calculates the weighted variance which is then used in the estimation step (E-step) of the EM algorithm
+#'
+#' @param x numeric vector
+#' @param w numeric vecotr of weights to be used in the caluclation of the weighted variance of x
+#' @param na.rm logical; remove NA values?
+#'
+#' @return numeric estimate of the weighted variance of x
+#' @export
+#' @importFrom stats weighted.mean
+#'
+#' @examples
+#' a <- 1:10
+#' b <- eq(0.1, 1, length.out = 10)
+#'
+#' weighted.var(a, b)
+
 weighted.var <- function(x, w, na.rm = FALSE) {
   if (na.rm) {
     w <- w[i <- !is.na(x)]
     x <- x[i]
   }
   sum.w <- sum(w)
-  (sum.w*(sum(w*(x-weighted.mean(x,w))^2))) / (sum.w^2 - sum(w^2))
+  rtn <- (sum.w*(sum(w*(x-weighted.mean(x,w))^2))) / (sum.w^2 - sum(w^2))
+  return(rtn)
 }
