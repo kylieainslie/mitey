f5f464f2f986ba9ed3406650d0ed60c15718a84b
Kylie Ainslie <ainslie.kylie@gmail.com>
2024-04-29 09:48:10 +0200

    initialise conv_tri_dist.R file

---
+++ R/conv_tri_dist.R
@@ -0,0 +1,15 @@
+#' Convolution of the triangular distribution with the mixture component density (continuous case)
+#'
+#' We split the folded normal distribution for Primary-Secondary, Primary-Tertiary and Primary-Quaternary routes into two parts
+#' + component 1: Co-Primary route
+#' + component 2+3: Primary-Secondary route
+#' + component 4+5: Primary-Tertiary route
+#' + component 6+7: Primary-Quaternary route
+#'
+#' @param
+#'
+#' @return
+#' @export
+#' @import
+#'
+#' @examples
