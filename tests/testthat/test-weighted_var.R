ee9ba1d57c62d9b86c9c17bb327c8f557c925476
Kylie Ainslie <ainslie.kylie@gmail.com>
2024-04-30 13:54:26 +0200

    add error message and test for when x and w are not the same length

--- tests/testthat/test-weighted_var.R
+++ tests/testthat/test-weighted_var.R
@@ -1,4 +1,9 @@
 test_that("check function arguments", {
   expect_error(weighted_var("a", 1:10))
   expect_error(weighted_var(1, "b"))
+  expect_error(weighted_var(1:11, seq(0.1, 1, length.out = 10)))
+})
+
+test_that("check output", {
+  expect_equal(weighted_var(1:10, seq(0.1, 1, length.out = 10)), 6.875)
 })
