4e9dcdfbc5294f25b5f3d1b45b4cc1b2a3f7f3ad
Kylie Ainslie <ainslie.kylie@gmail.com>
2024-05-01 14:31:55 +0200

    change default arguments for conv_tri_dist

--- tests/testthat/test-conv_tri_dist.R
+++ tests/testthat/test-conv_tri_dist.R
@@ -1,3 +1,9 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("check route argument", {
+  expect_error(conv_tri_dist(x = 1:10, sigma = 3, r = 10, mu = 5, route = 0))
+  expect_error(conv_tri_dist(x = 1:10, sigma = 3, r = 10, mu = 5, route = ""))
+})
+
+test_that("check quantity argument", {
+  expect_error(conv_tri_dist(x = 1:10, sigma = 3, r = 10, mu = 5, route = 1, quantity = 1))
+  expect_error(conv_tri_dist(x = 1:10, sigma = 3, r = 10, mu = 5, route = 1, quantity = ""))
 })
