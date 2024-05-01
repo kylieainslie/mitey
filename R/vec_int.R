890aae2c400f714391efd1cbe950c8f0ad98b711
Kylie Ainslie <ainslie.kylie@gmail.com>
2024-05-01 14:28:32 +0200

    include sigma and mu arguments to pass to conv_tri_dist()

--- R/vec_int.R
+++ R/vec_int.R
@@ -3,6 +3,8 @@
 #' @param x a (numeric) vector of index case to case intervals
 #' @param l lower bound for integration
 #' @param u upper bound for integration
+#' @param s standard deviation of distribution function to be integrated
+#' @param m mean of distribution function to be integrated
 #' @param rt route of transmission; integer from 1 to 7
 #' @param q character string; quantity to estimate, "zero", "lower", "upper"
 #'
@@ -11,13 +13,15 @@
 #' @importFrom stats integrate
 #'
 #' @examples
-#' my_data<-c(rep(1,38),rep(2,39),rep(3,30),rep(4,17),rep(5,7))
+#' df <-c(rep(1,38),rep(2,39),rep(3,30),rep(4,17),rep(5,7))
 #'
-#' vec_int(x = my_data, l = my_data, u = (my_data + 1), rt = 1, q = "zero")
+#' vec_int(x = df, l = df, u = (df + 1), s = sd(df), m = mean(df), rt = 1, q = "zero")
 
 # vectorized method
-vec_int <- Vectorize(function(x, l, u, rt, q) integrate(f = conv_tri_dist, lower = l, upper = u,
-                                      sigma = hsigma, r = x, mu = hmu, route = rt, quantity = q)[[1]])
+vec_int <- Vectorize(function(x, l, u, s, m, rt, q) integrate(f = conv_tri_dist, lower = l, upper = u,
+                                      sigma = s, r = x, mu = m, route = rt, quantity = q)[[1]])
+
+
 
 
 
