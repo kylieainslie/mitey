9c8ca42227bd03d20d2b3228c8e98364fce07eef
Kylie Ainslie <ainslie.kylie@gmail.com>
2024-06-24 12:29:57 +0200

    small formatting changes

--- R/weighted_var.R
+++ R/weighted_var.R
@@ -31,6 +31,6 @@ weighted_var <- function(x, w, na.rm = FALSE) {
     x <- x[i]
   }
   sum.w <- sum(w)
-  rtn <- (sum.w*(sum(w*(x-weighted.mean(x,w))^2))) / (sum.w^2 - sum(w^2))
+  rtn <- (sum.w * (sum(w * (x - weighted.mean(x, w))^2))) / (sum.w^2 - sum(w^2))
   return(rtn)
 }
