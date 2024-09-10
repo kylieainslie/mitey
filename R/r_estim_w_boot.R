ef7c1fc19ef3792c77ce4ce394657a5aa74b6d8b
Kylie Ainslie <ainslie.kylie@gmail.com>
2024-09-10 16:39:04 +0200

    uncount inc data before taking bootstrap samples, then rewrangle before running rt_estim()

--- R/r_estim_w_boot.R
+++ R/r_estim_w_boot.R
@@ -13,6 +13,8 @@
 #' @param n_bootstrap integer; number of bootstrap samples of the serial interval distribution
 #' @return A numeric vector with the expected reproduction number for each day.
 #' @export
+#' @import dplyr
+#' @import tidyr
 #' @importFrom purrr map
 #' @importFrom stats quantile
 #' @importFrom stats rnorm
@@ -20,11 +22,32 @@
 rt_estim_w_boot <- function(inc_dat, mean_si, sd_si, dist_si = "normal",
                             cut_tail = NULL, pos_only = TRUE, n_bootstrap = 100){
 
+  # uncount inc_dat, so that individual cases can be samples from
+  inc_dat_uncount <- inc_dat %>%
+    uncount(inc) %>%
+    mutate(inc = 1)
+
   # get bootstrap samples
-  boot_samples <- map(1:n_bootstrap, ~inc_dat[sample(nrow(inc_dat), replace = TRUE), ])
+  boot_samples <- map(1:n_bootstrap, ~inc_dat_uncount[sample(nrow(inc_dat_uncount), replace = TRUE), ])
+
+  # a little wrangling of each bootstrapped data frame:
+  # 1. order by onset_date
+  # 2. collapse rows with the same onset_date and increase the count of inc
+  # 3. complete the time series by filling in missing dates
+  # get all dates
+  all_dates <- seq(min(inc_dat$onset_date), max(inc_dat$onset_date), by = "day")
+
+  boot_samples_wrangled <- map(boot_samples, ~.x %>%
+                                arrange(onset_date) %>%
+                                group_by(onset_date) %>%
+                                summarise(inc = sum(inc), .groups = "drop") %>%
+                                complete(onset_date = all_dates,
+                                         fill = list(inc = 0)
+                                )
+                             )
 
   # Apply rt_estim to each bootstrap sample
-  boot_rt <- map(boot_samples, ~rt_estim(inc_dat = .x, mean_si = mean_si, sd_si = sd_si, dist_si = dist_si,
+  boot_rt <- map(boot_samples_wrangled, ~rt_estim(inc_dat = .x, mean_si = mean_si, sd_si = sd_si, dist_si = dist_si,
                                          cut_tail = cut_tail, pos_only = pos_only))
 
   # Calculate the mean R_t and confidence intervals across bootstrap samples
@@ -32,6 +55,6 @@ rt_estim_w_boot <- function(inc_dat, mean_si, sd_si, dist_si = "normal",
   rt_lower <- lapply(boot_rt, 2, quantile, probs = 0.025)
   rt_upper <- lapply(bootstrap_rt, 2, quantile, probs = 0.975)
 
-
+  return(lis)
 
 }
