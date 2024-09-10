137c46f4d6f1342e8e625e67726995f7e29298a6
Kylie Ainslie <ainslie.kylie@gmail.com>
2024-09-10 18:00:11 +0200

    initialise right_truncation_correction function

---
+++ R/right_truncation_correction.R
@@ -0,0 +1,18 @@
+#' Correct for right truncation in estimates of time-varying reproduction number
+#'
+#' This function calculates the correction factor for each observed time since infection (time_since_infection[i]). It uses the cumulative distribution function (`pnorm`) of the normal distribution to calculate the probability that the serial interval is less than the time lag T_now - t.
+#'
+#' @param t integer; time point of infection or symptom onset
+#' @param T_now interger; current time point
+#' @param mean_serial_interval numeric; mean of serial interval distribution
+#' @param sd_serial_interval numeric; standard deviation of serial interval distribution
+#' @return vector of correction weights
+#' @export
+#' @importFrom stats pnorm
+right_truncation_correction <- function(t, T_now, mean_serial_interval, sd_serial_interval) {
+  # Probability that the serial interval is less than the time lag (T - t)
+  prob <- pnorm((T_now - t - mean_serial_interval) / sd_serial_interval)
+  w <- 1 / prob
+  # Return the correction factor
+  return(w)  # Correction factor as per Cauchemez et al.
+}
