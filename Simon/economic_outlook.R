207754a4b2c0f3ebeb617a8659ccbdc27b2920bd
simon-carreel <carreelsimon@gmail.com>
2026-03-13 13:48:03 +1100

    essai

---
+++ Simon/economic_outlook.R
@@ -0,0 +1,55 @@
+library(tidyr)
+library(zoo)
+library(ggplot2)
+library(dplyr)
+
+setwd("C:/Users/carre/OneDrive/Bureau/Mitey_project/mitey/Simon")
+data<-read.csv("economic_dataset.csv")
+#récupérer le csv
+
+df<-as.data.frame(data)
+#le transformer en dataframe
+
+df[df==".."]<-"NA"
+#identifier les valeurs manquantes
+
+df$df_emptyness=rowSums(df=="NA")
+len<-ncol(data)
+df$df_completeness<-100 - df$df_emptyness/len*100
+
+df<-subset(df, df_completeness>10)
+#créer un df qui supprime les lignes avec trop de valeurs manquantes
+df$df_completeness<-NULL
+df$df_emptyness<-NULL
+df<-df[c(-2,-3,-4)]
+
+colnames(df)[2:ncol(df)] <- substr(colnames(df)[2:ncol(df)], 2, 7)
+
+df_long <- pivot_longer(
+  df,
+  cols = -Country,
+  names_to = "Quarter",
+  values_to = "Value"
+)
+
+df_long$Quarter<-as.Date(as.yearqtr(df_long$Quarter))
+df_long$Value<-as.numeric(df_long$Value)
+
+
+graph<-ggplot(df_long, aes(x=Quarter, y =Value, group=Country,color=Country))+
+  geom_line(show.legend = FALSE)+
+  ggtitle("Country's GDP in millions USD")
+print(graph)
+
+last_date<-max(df_long$Quarter)
+last_date
+
+quarters<-unique(df_long$Quarter)
+quarters
+
+
+last_record<-df_long %>% filter(Quarter=="2025-04-01")
+last_record %>%
+  arrange(desc(Value)) %>%
+
+slice(1:10)
--- Simon/essai2.R
+++ Simon/essai2.R
@@ -1,1 +1,29 @@
-x<-5
+setwd("C:/Users/carre/OneDrive/Bureau/Mitey_project/mitey/Simon")
+
+data<-read.csv("adult_train.csv")
+data[data==""]<-"NA"
+colors<-c("Male"="red", "Female"="blue")
+workclass<-unique(data$Workclass)
+symbol <-c(" State-gov"=0, " Self-emp-not-inc"=1," Private"=2," Federal-gov"=3,
+" Local-gov"=4,"NA"=5," Self-emp-inc"=6," Without-pay"=7," Never-worked"=8   )
+
+
+data$Sex <- trimws(data$Sex)       # supprime les espaces
+data$Sex <- factor(data$Sex)       # optionnel mais sécurise les niveaux
+#model<-lm(Hours_per_week  ~ Education_Num, data=data)
+#summary(model)
+
+#plot(data$Age,
+#     data$Capital_Gain,
+#     col=colors[data$Sex],
+#     pch = symbol[data$Workclass],
+#     main = "Capital gain as a function of Age",
+#     xlab = "Education",
+#     ylab = "Capital Gain")
+
+colnames(data)
+
+#model<-lm(Capital_Gain  ~ Education_Num+Hours_per_week, data=data)
+#summary(model)
+
+hist(data$Education_Num, data=data, breaks=20)
