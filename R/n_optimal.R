d79ab73af293b2c983031bc98ff1bb44380da489
simon-carreel <carreelsimon@gmail.com>
2026-04-30 11:27:48 +1000

    30/04

--- R/n_optimal.R
+++ R/n_optimal.R
@@ -4,13 +4,9 @@ n_optimal1 <- function(dat) {
 
 
 n_optimal2 <- function(dat) {
-  as.integer(sd(dat) / mean(dat)) + 2L  # +2 car minimum 2 routes
-}
-
-n_optimal3 <- function(dat) {
   as.integer(max(dat) / sd(dat))
 }
 
 #n_optimal1(icc_Influenza_Canada)
 #n_optimal2(icc_Influenza_Canada)
-#n_optimal3(icc_Influenza_Canada)
+
