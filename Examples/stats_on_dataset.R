25c283452d30f24efb1e18099a6534b4a4f266d3
Simon Carreel <101939974+simon-carreel@users.noreply.github.com>
2026-05-11 15:21:36 +1000

    deleted examples files before pull request

--- Examples/stats_on_dataset.R
+++ Examples/stats_on_dataset.R
@@ -7,10 +7,8 @@ stats <- validation_data %>%
     mean = mean(ICC_interval, na.rm = TRUE),
     sd = sd(ICC_interval, na.rm = TRUE),
     cv = sd / mean,
-    skewness = skewness(ICC_interval, na.rm = TRUE),
     median = median(ICC_interval, na.rm = TRUE),
     max = max(ICC_interval, na.rm = TRUE),
-    max_median_ratio = max / median,
     .groups = "drop"
   )
 
